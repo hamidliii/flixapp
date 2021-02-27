10a5cc626b43e52e790dc580523a5ce55de24bed
Nina Hamidli <nigar.hamidli1999@gmail.com>
2021-02-27 18:36:04 +0900

    Addition of grid layout view and movies details view.

---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by Nina on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+}
--- flixapp/MoviesViewController.swift
+++ flixapp/MoviesViewController.swift
@@ -68,4 +68,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
     
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        
+        // find the selected movie
+        let cell =  sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // pass the selected movie details view controller
+        let detailsViewController =  segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        // remove selection cell
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 }
